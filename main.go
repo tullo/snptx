af87385eec4343bbbc24a4cccb2a07c54f4dcd63
Andreas Amstutz <andreasamstutz@gmail.com>
2020-03-10 10:30:40 +0100

    restrict create route to respond to http posts only

--- main.go
+++ main.go
@@ -19,6 +19,12 @@ func showSnippet(w http.ResponseWriter, r *http.Request) {
 }
 
 func createSnippet(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		w.Header().Set("Allow", http.MethodPost)
+		http.Error(w, "Method Not Allowed", 405)
+		return
+	}
+
 	w.Write([]byte("Create a new snippet..."))
 }
 
