f4e63b4dc84df9c1f76a98fe654a92aaecabbd59
Andreas Amstutz <andreasamstutz@gmail.com>
2020-03-10 23:10:09 +0100

    add current year to footer

--- cmd/web/helpers.go
+++ cmd/web/helpers.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"net/http"
 	"runtime/debug"
+	"time"
 )
 
 func (app *application) serverError(w http.ResponseWriter, err error) {
@@ -22,6 +23,14 @@ func (app *application) notFound(w http.ResponseWriter) {
 	app.clientError(w, http.StatusNotFound)
 }
 
+func (app *application) addDefaultData(td *templateData, r *http.Request) *templateData {
+	if td == nil {
+		td = &templateData{}
+	}
+	td.CurrentYear = time.Now().Year()
+	return td
+}
+
 func (app *application) render(w http.ResponseWriter, r *http.Request, name string, data *templateData) {
 	ts, ok := app.templateCache[name]
 	if !ok {
@@ -31,7 +40,7 @@ func (app *application) render(w http.ResponseWriter, r *http.Request, name stri
 
 	// stage 1: write template into buffer
 	buf := new(bytes.Buffer)
-	err := ts.Execute(buf, data)
+	err := ts.Execute(buf, app.addDefaultData(data, r))
 	if err != nil {
 		app.serverError(w, err)
 		return
