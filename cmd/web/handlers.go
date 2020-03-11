2f8dea65d221b8808b1aef04e70928a720e70f72
Andreas Amstutz <andreasamstutz@gmail.com>
2020-03-11 11:40:18 +0100

    form data: validate the different types of inputs

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"net/http"
 	"strconv"
+	"strings"
+	"unicode/utf8"
 
 	"github.com/tullo/snptx/pkg/models"
 )
@@ -63,6 +65,37 @@ func (app *application) createSnippet(w http.ResponseWriter, r *http.Request) {
 	title := r.PostForm.Get("title")
 	content := r.PostForm.Get("content")
 	expires := r.PostForm.Get("expires")
+
+	errors := make(map[string]string)
+
+	// title field must not be blank and not more than 100 characters long
+	if strings.TrimSpace(title) == "" {
+		errors["title"] = "This field cannot be blank"
+	} else if utf8.RuneCountInString(title) > 100 {
+		errors["title"] = "This field is too long (maximum is 100 characters)"
+	}
+
+	// content field must not be blank
+	if strings.TrimSpace(content) == "" {
+		errors["content"] = "This field cannot be blank"
+	}
+
+	// expires field must not be blank and
+	// matches one of the permitted values ("1", "7" or "365").
+	if strings.TrimSpace(expires) == "" {
+		errors["expires"] = "This field cannot be blank"
+	} else if expires != "365" && expires != "7" && expires != "1" {
+		errors["expires"] = "This field is invalid"
+	}
+
+	// if there are any errors, dump them in a plain text HTTP response and return from the handle
+	if len(errors) > 0 {
+		app.render(w, r, "create.page.tmpl", &templateData{
+			FormErrors: errors,
+			FormData:   r.PostForm,
+		})
+		return
+	}
 
 	// create a new snippet record in the database using the form data
 	id, err := app.snippets.Insert(title, content, expires)
