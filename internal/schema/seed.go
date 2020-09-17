e0c2db5e8aa10a49aca2501a4081aae066c11bd1
Andreas Amstutz <tullo@users.noreply.github.com>
2020-09-18 01:16:18 +0200

    update seed data with argon2 hashes

--- internal/schema/seed.go
+++ internal/schema/seed.go
@@ -40,7 +40,7 @@ const seeds = `
 
 	-- Create admin and regular User with password "gophers"
 	INSERT INTO users (user_id, name, email, roles, password_hash, date_created, date_updated) VALUES
-		('5cf37266-3473-4006-984f-9325122678b7', 'Admin Gopher', 'admin@example.com', '{ADMIN,USER}', '$2a$10$1ggfMVZV6Js0ybvJufLRUOWHS5f6KneuP0XwwHpJ8L8ipdry9f2/a', '2019-03-24 00:00:00', '2019-03-24 00:00:00'),
-		('45b5fbd3-755f-4379-8f07-a58d4a30fa2f', 'User Gopher', 'user@example.com', '{USER}', '$2a$10$9/XASPKBbJKVfCAZKDH.UuhsuALDr5vVm6VrYA9VFR8rccK86C1hW', '2019-03-24 00:00:00', '2019-03-24 00:00:00')
+		('5cf37266-3473-4006-984f-9325122678b7', 'Admin Gopher', 'admin@example.com', '{ADMIN,USER}', '$argon2id$v=19$m=65536,t=1,p=2$HG6d1eJeIhZ40X7+jwNDng$DDUhwjMbLNiK53wg2xcEPjJusnhBCDUbQYVYYNnkKP0', '2019-03-24 00:00:00', '2019-03-24 00:00:00'),
+		('45b5fbd3-755f-4379-8f07-a58d4a30fa2f', 'User Gopher', 'user@example.com', '{USER}', '$argon2id$v=19$m=65536,t=1,p=2$8yweayOAy+aZK/1egvrbEA$OA5QdHwK0rL+dWhWY/1lsv9G1zKF+gaS3yjwZvw4OTI', '2019-03-24 00:00:00', '2019-03-24 00:00:00')
 		ON CONFLICT DO NOTHING;
 `
