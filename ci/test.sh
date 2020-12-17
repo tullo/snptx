2039cd6589cad639f6106a2aa4d4d3e86de0c115
Andreas Amstutz <tullo@users.noreply.github.com>
2020-12-17 20:04:18 +0100

    upload coverage reports

---
+++ ci/test.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+set -e
+echo "" > coverprofile.cov
+
+go test -mod=vendor -failfast -test.timeout=90s \
+    -covermode=count -coverprofile=coverprofile.cov -run="^Test" \
+    -coverpkg=$(go list -mod=vendor ./... | grep -v "/test" | tr '\n' ',') \
+    ./...
+# go tool cover -func=coverprofile.cov
+# go tool cover -html=coverprofile.cov
