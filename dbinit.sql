c63d09282a166cf11704ae3050174fde103f8f7a
Andreas Amstutz <tullo@users.noreply.github.com>
2025-04-16 00:12:27 +0200

    add db init sql

---
+++ dbinit.sql
@@ -0,0 +1,9 @@
+CREATE DATABASE snptx WITH ENCODING 'utf8';
+
+-- CREATE USER 'web'@'localhost';
+-- GRANT CREATE, DROP, ALTER, INDEX, SELECT, INSERT, UPDATE, DELETE ON snptx.* TO 'web'@'localhost';
+-- ALTER USER 'web'@'localhost' IDENTIFIED BY 'pass';
+
+-- CREATE USER 'web'@'192.168.0.170';
+-- GRANT CREATE, DROP, ALTER, INDEX, SELECT, INSERT, UPDATE, DELETE ON snptx.* TO 'web'@'192.168.0.170';
+-- ALTER USER 'web'@'192.168.0.170' IDENTIFIED BY 'pass';
